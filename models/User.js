8fc6fea42ef35b20ee7c06862e19e6261b9c9ee2
Your Name <you@example.com>
2022-10-28 14:51:41 -0400

    created user routes and tested user routes

--- models/User.js
+++ models/User.js
@@ -3,37 +3,41 @@ const sequelize = require("../config/connection");
 
 class User extends Model {}
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
     },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [4],
-    },
-  },
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "user",
-});
+  {
+    sequelize: sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const routes = require("./controllers");
+// const path = require("path");
+
 const sequelize = require("./config/connection");
 
 const app = express();
@@ -6,6 +9,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now Listening"));
 });
