285bf227916aff30e8e0ac22f5a11c7e739861a7
Your Name <you@example.com>
2022-10-28 18:30:34 -0400

    created routes to serve homepage though handlebars and passed data from the database to render back on the page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,35 @@
 const router = require("express").Router();
-// const { User, Post, Comment } = require("../../models");
+const { User, Post, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    order: [["created_at", "DESC"]],
+    // include: [
+    //   {
+    //     model: User,
+    //     attributes: ["username"],
+    //   },
+    //   {
+    //     model: Comment,
+    //     attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+    //     include: {
+    //       model: User,
+    //       attributes: ["username"],
+    //     },
+    //   },
+    // ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
