a844434bc6cc2e00e89d2ab6b32f887b8cab1ab5
Your Name <you@example.com>
2022-11-01 18:01:05 -0400

    created dashboard and routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+
+const sequelize = require("../config/connection");
+
+router.get("/", (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", {
+        posts,
+        username: req.session.username,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
