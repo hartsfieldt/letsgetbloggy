be9aa6b0f3f2738f24153d23ddec4d322128d77a
Your Name <you@example.com>
2022-11-01 11:01:10 -0400

    check the session to make sure that the user is logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: ["id", "title", "content", "created_at"],
     order: [["created_at", "DESC"]],
@@ -33,6 +34,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
   res.render("login");
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
-  secret: "my secret",
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
