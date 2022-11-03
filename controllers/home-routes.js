23688818863ebe40abb91d426e1678d8d7d35457
Your Name <you@example.com>
2022-11-02 21:22:58 -0400

    created function to edit post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,20 +6,20 @@ router.get("/", (req, res) => {
   Post.findAll({
     attributes: ["id", "title", "content", "created_at"],
     order: [["created_at", "DESC"]],
-    // include: [
-    //   {
-    //     model: User,
-    //     attributes: ["username"],
-    //   },
-    //   {
-    //     model: Comment,
-    //     attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
-    //     include: {
-    //       model: User,
-    //       attributes: ["username"],
-    //     },
-    //   },
-    // ],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
