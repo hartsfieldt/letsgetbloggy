a79beaac59406a79f51605bac5d3123c7e819615
Your Name <you@example.com>
2022-11-02 21:42:11 -0400

    editted page to refresh when a user signs up

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -17,7 +17,7 @@ const signUpFormHandler = async (event) => {
       headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
-      console.log("success");
+      document.location.replace("/");
     } else {
       alert(response.statusText);
     }
